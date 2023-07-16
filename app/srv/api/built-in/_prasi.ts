1c0c3035de0cbd7249b0f857ff2a0aa230d51ecc
Rizky <rizky05@gmail.com>
2023-07-16 11:00:51 +0700

    wip fix

--- app/srv/api/built-in/_prasi.ts
+++ app/srv/api/built-in/_prasi.ts
@@ -1,5 +1,5 @@
 import { dir } from "dir";
-import { existsAsync, readAsync } from "fs-jetpack";
+import { existsAsync, readAsync, writeAsync } from "fs-jetpack";
 import { apiContext } from "service-srv";
 const cache = {} as Record<string, string>;
 
@@ -21,14 +21,39 @@ export const _ = {
           res.send(await readAsync(pdir, "utf8"));
         }
       },
-      api: async () => {
+      "api-types": async () => {
         const pdir = dir.path("srv/api.d.ts");
         if (await existsAsync(pdir)) {
           res.type("text/x.typescript");
-          res.send(await readAsync(pdir, "utf8"));
+
+          let result = await readAsync(pdir, "utf8");
+          if (result) {
+            if (!result.startsWith("/* DB-CLEANED */")) {
+              const parr = result.split("\n");
+              for (const [k, v] of Object.entries(parr)) {
+                if (v.trim().startsWith("const db: WebGlobal")) {
+                  parr[k] = "";
+                }
+              }
+              parr.unshift("/* DB-CLEANED */");
+              result = parr.join("\n");
+              await writeAsync(pdir, result);
+            }
+          }
+
+          res.send(result);
         }
       },
-    }[req.params._];
+      "api-entry": async () => {
+        try {
+          const apiEntry = await import("../../../../app/gen/srv/api/entry");
+          res.type("application/json");
+          res.send(JSON.stringify(apiEntry));
+        } catch (e) {
+          return "API entry not found";
+        }
+      },
+    }[req.params._.split("/")[0]];
 
     if (action) await action();
   },
--- app/srv/main.ts
+++ app/srv/main.ts
@@ -1,9 +1,14 @@
 import { createAPIServer } from "service-srv";
 import "../../pkgs/service/pkgs/service-web/pkgs/web-init/src/types";
 
+let port = 4500;
+try {
+  port = require("./port").port;
+} catch (E) {}
+
 export const main = createAPIServer({
   name: "srv",
-  port: 4500,
+  port,
   // make sure cookieKey is different for each app
   // changing cookie key, will force all user to log out
   cookieKey: `ryl-sid-JgvCz3F4K6pfPNwM`,
