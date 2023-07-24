424ddae3eeae639e741364cb808af0263f441b14
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-07-24 12:13:12 +0700

    Added Auth Middleware

--- app/srv/api/service/test/handler/test-handler.ts
+++ app/srv/api/service/test/handler/test-handler.ts
@@ -1,9 +1,15 @@
 import { apiContext } from "service-srv";
+import { ResponseFormatter } from "../../../core/network-result/response-formatter";
+import { authMiddleware } from "../../../core/utils/auth-middleware";
 export const _ = {
-  url: "/test",
+  url: "/api/test",
   async api() {
     const { req, res } = apiContext(this);
-
+    const loggedIn = await authMiddleware(req, res);
+    return loggedIn;
+    if (!loggedIn) {
+      return ResponseFormatter.error(null, "Unauthenticated", 401);
+    }
     return "hello world";
   },
 };
