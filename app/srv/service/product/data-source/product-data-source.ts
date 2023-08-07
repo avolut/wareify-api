3a8aea628489841faf1d4998833df0c2c4269bb0
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-08-07 16:02:43 +0700

    Added list product by organization

---
+++ app/srv/service/product/data-source/product-data-source.ts
@@ -0,0 +1,76 @@
+import dayjs from "dayjs";
+import { ProductEntity } from "../entity/product-entity";
+import { PrismaClient } from "../../../../../app/db/node_modules/.gen";
+import { global } from "../../../global";
+
+export interface IProductDataSource {
+  getProductsByOrganizationId(organizationId: number): Promise<ProductEntity[]>;
+  getProductById(id: number): Promise<ProductEntity>;
+  getProductBySlug(slug: string): Promise<ProductEntity>;
+  getProductByType(type: number): Promise<ProductEntity[]>;
+}
+
+export class ProductDataSource implements IProductDataSource {
+  private timestamps: object;
+  constructor(private prisma: PrismaClient, private now: Date) {
+    this.timestamps = {
+      createdAt: this.now,
+      updatedAt: this.now,
+    };
+  }
+
+  public async getProductsByOrganizationId(organizationId: number): Promise<ProductEntity[]> {
+    const products = await this.prisma.product.findMany({
+      where: { organizationId: organizationId },
+    });
+    
+    return Promise.all(products.map((product) => this.mapToProductEntity(product)));
+  }
+
+  public async getProductById(id: number): Promise<ProductEntity> {
+    const product = await this.prisma.product.findUnique({
+      where: { id: id },
+    });
+
+    return this.mapToProductEntity(product);
+  }
+
+  public async getProductBySlug(slug: string): Promise<ProductEntity> {
+    const product = await this.prisma.product.findUnique({
+      where: { slug: slug },
+    });
+
+    return this.mapToProductEntity(product);
+  }
+
+  public async getProductByType(type: number): Promise<ProductEntity[]> {
+    const products = await this.prisma.product.findMany({
+      where: { productTypeId: type },
+    });
+
+    return Promise.all(products.map((product) => this.mapToProductEntity(product)));
+  }
+
+  private async mapToProductEntity(product: any): Promise<ProductEntity> {
+    return new ProductEntity(
+      product.id,
+      product.name,
+      product.slug,
+      product.description,
+      product.sku,
+      product.code,
+      product.uom,
+      product.type,
+      product.photo,
+      product.createdAt,
+      product.updatedAt
+    );
+  }
+}
+
+export class ProductDataSourceFactory {
+  static create(): IProductDataSource {
+    return new ProductDataSource(new PrismaClient(),
+    dayjs().add(parseInt(global.UTC_TIMEZONE.toString()), "hour").toDate());
+  }
+}
---
+++ app/srv/service/product/repository/product-repository.ts
@@ -0,0 +1,35 @@
+import { IProductDataSource, ProductDataSourceFactory } from "../data-source/product-data-source";
+import { ProductEntity } from "../entity/product-entity";
+
+export interface IProductRepository {
+  getProductsByOrganizationId(organizationId: number): Promise<ProductEntity[]>;
+  getProductById(id: number): Promise<ProductEntity>;
+  getProductBySlug(slug: string): Promise<ProductEntity>;
+  getProductByType(type: number): Promise<ProductEntity[]>;
+}
+
+export class ProductRepositoryFactory {
+  static create(): IProductRepository {
+    return new ProductRepository(ProductDataSourceFactory.create());
+  }
+}
+
+class ProductRepository implements IProductRepository {
+  constructor(private productDataSource: IProductDataSource) {}
+
+  async getProductsByOrganizationId(organizationId: number): Promise<ProductEntity[]> {
+    return this.productDataSource.getProductsByOrganizationId(organizationId);
+  }
+
+  async getProductById(id: number): Promise<ProductEntity> {
+    return this.productDataSource.getProductById(id);
+  }
+
+  async getProductBySlug(slug: string): Promise<ProductEntity> {
+    return this.productDataSource.getProductBySlug(slug);
+  }
+
+  async getProductByType(type: number): Promise<ProductEntity[]> {
+    return this.productDataSource.getProductByType(type);
+  }
+}
