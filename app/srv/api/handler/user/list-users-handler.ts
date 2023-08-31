0f1c30c63341883117ef09561a7a8471b5d19ce8
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2023-08-31 11:44:20 +0700

    Change params on some handlers

--- app/srv/api/handler/user/list-users-handler.ts
+++ app/srv/api/handler/user/list-users-handler.ts
@@ -1,28 +1,34 @@
 import { apiContext } from "service-srv";
 import { ResponseFormatter } from "../../../core/network-result/response-formatter";
 import { authMiddleware } from "../../../core/utils/auth-middleware";
-import { IListUsersByRoleUseCase, ListUsersByRoleUseCaseFactory } from "../../../service/user/use-case/list-users-by-role-use-case";
-import { IListUsersUseCase, ListUsersUseCaseFactory } from "../../../service/user/use-case/list-users-use-case";
+import {
+  IListUsersByRoleUseCase,
+  ListUsersByRoleUseCaseFactory,
+} from "../../../service/user/use-case/list-users-by-role-use-case";
+import {
+  IListUsersUseCase,
+  ListUsersUseCaseFactory,
+} from "../../../service/user/use-case/list-users-use-case";
 export const _ = {
   url: "/api/users/",
-  async api() {
+  async api(roleParams?: string) {
     const { req, res } = apiContext(this);
     const loggedIn = await authMiddleware(req, res);
     if (!loggedIn) {
       return ResponseFormatter.error(null, "Unauthenticated", 401);
     }
     try {
-      const role = req.query.role;
-      if(role) {
+      const role = roleParams;
+      if (role) {
         const listUsersByRoleUseCase: IListUsersByRoleUseCase =
-        ListUsersByRoleUseCaseFactory.create();
+          ListUsersByRoleUseCaseFactory.create();
         const users = await listUsersByRoleUseCase.execute({
           role: role as string,
         });
         return ResponseFormatter.success(users, "List users success");
       } else {
-        const listUsersUseCase: IListUsersUseCase = 
-        ListUsersUseCaseFactory.create();
+        const listUsersUseCase: IListUsersUseCase =
+          ListUsersUseCaseFactory.create();
         const users = await listUsersUseCase.execute();
         return ResponseFormatter.success(users, "List users success");
       }
